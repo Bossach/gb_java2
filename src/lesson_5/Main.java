bf4c65c2f4a9519be0e7658f1c1e2d0823b88c98
bossach <boss52@inbox.ru>
2020-04-14 22:08:24 +0300

    lesson 5 Debug

--- src/lesson_5/Main.java
+++ src/lesson_5/Main.java
@@ -5,26 +5,37 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int size = 10000000;
-        float[] arr = new float[size];
+        int size = 10;//000000;
+        float[] globArr = new float[size];
 
-        Arrays.fill(arr, 1);
+        Arrays.fill(globArr, 1);
 
-        singleThreadProcess(arr);
+        System.out.println("\tMain aray from main() :" + Arrays.toString(globArr));
 
-        Arrays.fill(arr, 1);
+        singleThreadProcess(globArr);
 
-        multiThreadProcess(arr, 2);
+        System.out.println("\tMain aray from main() :" + Arrays.toString(globArr));
 
-        Arrays.fill(arr, 1);
+        Arrays.fill(globArr, 1);
 
-        multiThreadProcess(arr, 64);    //лучший результат на моём компьютере (из степеней двойки)
+        multiThreadProcess(globArr, 2);
+
+        System.out.println("\tMain aray from main() :" + Arrays.toString(globArr));
+
+        Arrays.fill(globArr, 1);
+
+        multiThreadProcess(globArr, 64);    //лучший результат на моём компьютере (из степеней двойки)
+    }
+
+    private static void process(float[] arr, int bias) {
+        for (int i = 0; i < arr.length; i++) {
+            int realI = i + bias;   //вычисляем позицию элемента в изначальном массиве для коректного рассчёта
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + realI / 5) * Math.cos(0.2f + realI / 5) * Math.cos(0.4f + realI / 2));
+        }
     }
 
     private static void process(float[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
+        process(arr, 0);
     }
 
     private static void singleThreadProcess(float[] arr) {
@@ -37,6 +48,9 @@ public class Main {
         long endTime = System.currentTimeMillis();
 
         System.out.printf("%dms%n", endTime - startTime);
+
+        //Debug
+        System.out.println("\tMain array print from singleThreadProcess() :" + Arrays.toString(arr));
     }
 
     private static void multiThreadProcess(float[] arr, int threadsCount) {
@@ -55,16 +69,18 @@ public class Main {
 
         for (int i = 0; i < threadsCount; i++) {
             float[] tempArray = splittedArrays[i];    //чтобы запизнуть в лямбду, i не хотела пропихиваться
-            threads[i] = new Thread(() -> process(tempArray));
+            int bias = i * arr.length / threadsCount;   // Учитываем смещение для корректного рассчёта
+            threads[i] = new Thread(() -> process(tempArray, bias));
             threads[i].start();
         }
 
         //Ждём все потоки
-        boolean isComplete;
-
-      
             for (Thread thread : threads) {
-               thread.join();
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
             }
         //
 
@@ -74,6 +90,8 @@ public class Main {
 
         System.out.printf("%d threads - %dms%n", threadsCount, endTime - startTime);
 
+        //Debug
+        System.out.println("\tMain array print from multiThreadProcess() :" + Arrays.toString(arr));
     }
 
     private static float[][] splitFloatArray(float[] sourceArray, int partsCount) {
